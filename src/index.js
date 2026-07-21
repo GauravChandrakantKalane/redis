627656a1889f13867d624f7a370b39b767ac6f62
GauravChandrakantKalane <gauravkalane28@gmail.com>
2026-07-21 18:44:49 +0530

    implemented simple redis banner api's

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import Redis from "ioredis";
+import { BANNER_KEY } from "./util/keys.js";
 
 
 
 const app = express();
+app.use(express.json());
 
 const redis = new Redis({
     url:"redis://localhost:6379"
@@ -22,6 +24,37 @@ app.get("/mongo", async (req, res) => {
     }
     res.json({message:`MongoDB connected successfully`})
 })
+
+app.post("/banner", async (req, res) => {
+    await redis.set(BANNER_KEY, req.body.message || "Welcome")
+    res.json({
+        success:true
+    })
+})
+
+
+app.get("/banner", async (req, res) => {
+    const data = await redis.get(BANNER_KEY)
+    res.json({
+        success:true,
+        data
+    })
+})
+
+app.delete("/banner", async (req, res) => {
+    await redis.del(BANNER_KEY)
+    res.json({
+        success:true
+    })
+})
+
+app.get("/banner/exists", async (req, res) => {
+    const result = await redis.exists(BANNER_KEY)
+    res.json({ 
+        data:result
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
